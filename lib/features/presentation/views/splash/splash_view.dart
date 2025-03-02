17a6855cea14bbbf586dfb97be2ad732b098372f
Gihan Dhananjaya <76836282+GihanDhananjaya@users.noreply.github.com>
2025-03-02 21:02:05 +0530

    changed SignIn view

--- lib/features/presentation/views/splash/splash_view.dart
+++ lib/features/presentation/views/splash/splash_view.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../../../../core/service/dependency_injection.dart';
 import '../../../../error/messages.dart';
 import '../../../../utils/app_colors.dart';
